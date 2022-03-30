a28caf7077ca3302c582279bda58e24ed352ab30
Alexandre Costa <alexandreodpc@gmail.com>
2022-03-30 20:55:16 -0300

    inicio

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import reactDom from "react-dom";
+
+import "./assets/css/reset.css"
+import "./assets/css/style.css"
+
+import App from "./components/App"
+
+ReactDOM.render(<App />, document.querySelector(".root"))
